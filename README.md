b4822e95bc28a6cceb42bbb92ac9f5115b35eef2
Chiranjit Poddar <chiranjit.connect@gmail.com>
2024-10-02 05:55:39 -0400

    Added search - all the fiels are required

--- README.md
+++ README.md
@@ -23,3 +23,9 @@ npm start
 ```
 
 Open [http://localhost:3000](http://localhost:3000) in your browser OR use a API client like [POSTMAN](https://www.postman.com/downloads/).
+
+## URL Request to test the code
+
+```bash
+http://localhost:3000/search?departure=2017-06-01T21:21:17.274Z&arrival=2017-06-01T22:21:17.274Z&duration=10&carrier=FR
+```
--- controllers/seachController.js
+++ controllers/seachController.js
@@ -1,41 +1,53 @@
 import axios from 'axios';
 export const getSearchFlight = async (req, res) => {
-  const { departure, arrival, duration, carrier: reqCarrier } = req.query;
+  const {
+    departure,
+    arrival,
+    duration: reqDuration,
+    carrier: reqCarrier,
+  } = req.query;
   const { data: flights } = await axios.get(
     'https://gist.githubusercontent.com/bgdavidx/132a9e3b9c70897bc07cfa5ca25747be/raw/8dbbe1db38087fad4a8c8ade48e741d6fad8c872/gistfile1.txt',
   );
 
-  const filterFlightsByTime = flights.reduce((prev, curr) => {
-    const searchDepart = new Date(curr.departureTime).getMilliseconds();
-    const resArrival = new Date(curr.arrivalTime).getMilliseconds();
-    const reqDepart = new Date(departure).getMilliseconds();
-    const reqArrival = new Date(arrival).getMilliseconds();
+  const reqDepart = new Date(departure).getTime();
+  const reqArrival = new Date(arrival).getTime();
 
-    const resDurationtion = (resArrival - searchDepart) / 1000 / 60 / 60;
-    const score =
-      resDurationtion * (reqCarrier ? 1 : 0.9) + getDistanceBetweenAirports;
+  console.log('1----- : ', flights.length);
+  const filterFlights = flights.reduce((prev, curr) => {
+    const receivedDepart = new Date(curr.departureTime).getTime();
+    const receivedArrival = new Date(curr.arrivalTime).getTime();
+
+    const receivedDuration = Math.floor(
+      (receivedArrival - receivedDepart) / (1000 * 60 * 60),
+    );
 
     if (
-      searchDepart >= reqDepart &&
-      resArrival <= reqArrival &&
-      duration >= resDurationtion
+      receivedDepart >= reqDepart &&
+      receivedDepart <= reqArrival &&
+      (!reqDuration || reqDuration >= receivedDuration)
     ) {
-      prev = [...prev, { ...curr, resDurationtion, score }];
+      // console.log("3 --- : ", receivedDuration);
+      const score =
+        receivedDuration * (reqCarrier ? 1 : 0.9) +
+        getDistanceBetweenAirports();
+      prev = [...prev, { ...curr, duration: receivedDuration, score }];
     }
     return prev;
   }, []);
 
-  const filterFlightsByCarrier = filterFlightsByTime.filter(
-    (flight) => flight.carrier === carrier,
+  const filterFlightsByCarrier = filterFlights.filter(
+    (flight) => flight.carrier === reqCarrier,
   );
 
+  console.log('2----- : ', filterFlightsByCarrier.length);
   const sortedByScore = filterFlightsByCarrier.sort(
-    (a, b) => a.score < b.score,
+    (a, b) => a.score - b.score,
   );
 
   return res.send(sortedByScore);
 };
 
-const getDistanceBetweenAirports = async (code1, code2) => {
+const getDistanceBetweenAirports = (code1, code2) => {
   return 200;
 };
